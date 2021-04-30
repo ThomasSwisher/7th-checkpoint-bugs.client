54c6afde102bf9d6ac4f9969a0c783bed86491d1
Thomas Swisher <thomas@structuredseo.com>
2021-04-30 15:19:57 -0600

    init

---
+++ src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'dev-2rkcetm8.us.auth0.com'
+export const audience = 'https://kanban'
+export const clientId = 'cNi80sCnHUKeT7YEsq5zX3ltbqShklVH'
